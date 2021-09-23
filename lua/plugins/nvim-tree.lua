9e58b960b20f4b5e7efaa9e3c01d3761b3b78dd6
Zachary Churchill <zacharyachurchill@gmail.com>
2021-09-23 15:09:13 -0400

    don't hide dotfiles

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,7 +2,6 @@ return function()
     vim.g.nvim_tree_ignore = {'.git', 'node_modules', '.cache'}
     vim.g.nvim_tree_follow = 1
     vim.g.nvim_tree_indent_markers = 1
-    vim.g.nvim_tree_hide_dotfiles = 1
 
     local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 
