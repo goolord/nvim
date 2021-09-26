e0e65d0f693da5ddf9720caddb81e41234b90726
Zachary Churchill <zacharyachurchill@gmail.com>
2021-09-26 10:43:10 -0400

    faster timeoutlen

--- lua/settings.lua
+++ lua/settings.lua
@@ -24,7 +24,7 @@ vim.opt.mouse         = 'a'
 vim.opt.scrolloff     = 5
 vim.opt.shortmess     = 'atToOc'
 vim.opt.sidescrolloff = 10
-vim.opt.timeoutlen    = 500
+vim.opt.timeoutlen    = 400
 vim.opt.ttimeoutlen   = 50
 -- tabs
 vim.opt.list          = true
