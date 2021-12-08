a6697e8777765baa4d35dd1dd39eb45bfcb4ab59
Zachary Churchill <zacharyachurchill@gmail.com>
2021-12-08 14:34:34 -0500

    which_key

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -24,6 +24,14 @@ return function()
             },
             selection_caret = ' ',
             entry_prefix = '  ',
+            mappings = {
+                i = {
+                    -- map actions.which_key to <C-h> (default: <C-/>)
+                    -- actions.which_key shows the mappings for your picker,
+                    -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+                    ["<C-h>"] = "which_key"
+                },
+            },
         },
 
         extensions = {
