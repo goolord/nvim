2087fd59c823f354c03c9fa45cc6159c36b2d9c0
Zachary Churchill <zacharyachurchill@gmail.com>
2022-01-17 19:29:51 -0500

    more statusline updates

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,45 +1,47 @@
 return function()
     local dark_bg = '#1d2021'
+    local function mode(x) return { bg = dark_bg, fg = x, gui = 'bold' } end
+    local def = { bg = '#3c3836', fg = vim.g.terminal_color_15 }
     local custom_gruvbox = {
         normal = {
-            a = { bg = dark_bg , fg = vim.g.terminal_color_15, gui = 'bold' },
-            b = { bg = vim.g.terminal_color_0, fg = vim.g.terminal_color_15 },
-            c = { bg = vim.g.terminal_color_0, fg = vim.g.terminal_color_15 },
+            a = mode(vim.g.terminal_color_15),
+            b = def,
+            c = def,
         },
         insert = {
-            a = { bg = dark_bg, fg = vim.g.terminal_color_14, gui = 'bold' },
-            b = { bg = vim.g.terminal_color_0, fg = vim.g.terminal_color_15 },
-            c = { bg = vim.g.terminal_color_0, fg = vim.g.terminal_color_15 },
+            a = mode(vim.g.terminal_color_14),
+            b = def,
+            c = def,
         },
         visual = {
-            a = { bg = dark_bg, fg = vim.g.terminal_color_11, gui = 'bold' },
-            b = { bg = vim.g.terminal_color_0, fg = vim.g.terminal_color_15 },
-            c = { bg = vim.g.terminal_color_0, fg = vim.g.terminal_color_15 },
+            a = mode(vim.g.terminal_color_11),
+            b = def,
+            c = def,
         },
         replace = {
-            a = { bg = dark_bg, fg = vim.g.terminal_color_9, gui = 'bold' },
-            b = { bg = vim.g.terminal_color_0, fg = vim.g.terminal_color_15 },
-            c = { bg = vim.g.terminal_color_0, fg = vim.g.terminal_color_15 },
+            a = mode(vim.g.terminal_color_9),
+            b = def,
+            c = def,
         },
         command = {
-            a = { bg = dark_bg, fg = vim.g.terminal_color_13, gui = 'bold' },
-            b = { bg = vim.g.terminal_color_0, fg = vim.g.terminal_color_15 },
-            c = { bg = vim.g.terminal_color_0, fg = vim.g.terminal_color_15 },
+            a = mode(vim.g.terminal_color_13),
+            b = def,
+            c = def,
         },
         inactive = {
-            a = { bg = dark_bg, fg = vim.g.terminal_color_7, gui = 'bold' },
-            b = { bg = vim.g.terminal_color_0, fg = vim.g.terminal_color_15 },
-            c = { bg = vim.g.terminal_color_0, fg = vim.g.terminal_color_15 },
+            a = mode(vim.g.terminal_color_7),
+            b = def,
+            c = def,
         },
     }
 
     local config = require('lualine').get_config()
     config.options.theme = custom_gruvbox
-    config.options.component_separators = {left = '|', right = '|'}
+    config.options.component_separators = {left = ' ', right = ''}
     config.options.section_separators = {left = '', right = ''}
     config.options.icons_enabled = true
     config.extensions = { 'fugitive', 'nvim-tree' }
-    config.sections.lualine_b[2] = {'diff', symbols = { added = ' ', modified = '柳 ', removed = ' ' }}
+    config.sections.lualine_b[2] = {'diff', symbols = { added = ' ', modified = '柳', removed = ' ' }}
 
     require('lualine').setup(config)
 end
