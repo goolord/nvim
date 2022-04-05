d3bbe005c9d3ab9a0b063ebb0e8848df9896b77b
Zachary Churchill <zacharyachurchill@gmail.com>
2022-04-04 22:54:27 -0400

    completion

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -19,34 +19,33 @@ return function()
             end
         },
 
-        completion = {
-            trim_match = false,
-        },
-
         mapping = {
             ['<C-p>'] = cmp.mapping.select_prev_item(),
             ['<C-n>'] = cmp.mapping.select_next_item(),
-            ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
-            ['<C-Space>'] = cmp.mapping.complete(),
-            ['<C-e>'] = cmp.mapping.close(),
-            ['<CR>'] = cmp.mapping.confirm({
-                behavior = cmp.ConfirmBehavior.Insert,
-                select = false,
+            ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+            ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
+            ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
+            ['<C-e>'] = cmp.mapping({
+                i = cmp.mapping.abort(),
+                c = cmp.mapping.close(),
             }),
+            ['<CR>'] = cmp.mapping.confirm({ select = false }),
             ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
             ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' })
         },
 
         sources = {
-            -- { name = 'buffer' },
             { name = 'nvim_lsp' },
+            -- { name = 'buffer' },
             { name = 'cmp_tabnine' },
             -- { name = 'emoji' },
             -- { name = 'tags' },
             { name = 'path' },
             { name = 'vsnip' },
         };
+
+        experimental = { ghost_text = true },
+        -- view = { entries = "native" },
 
         enabled = function ()
             return vim.o.bt == ''
