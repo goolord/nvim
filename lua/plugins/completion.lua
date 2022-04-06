08c095bfe278c7e545599dc729c2b7b6e35bdac1
Zachary Churchill <zacharyachurchill@gmail.com>
2022-04-06 14:16:56 -0400

    disable ghost text

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -44,7 +44,7 @@ return function()
             { name = 'vsnip' },
         };
 
-        experimental = { ghost_text = true },
+        -- experimental = { ghost_text = true },
         -- view = { entries = "native" },
 
         enabled = function ()
