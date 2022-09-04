d1bf4ba2022e4859964e8a26996c6637582d6d97
Zachary Churchill <zacharyachurchill@gmail.com>
2022-09-04 01:40:15 -0400

    rm gruvbox8

--- lua/plugins/gruvbox.lua
+++
@@ -1,8 +0,0 @@
-return function()
-    vim.g.gruvbox_bold = 1
-    vim.g.gruvbox_filetype_hi_groups = 1
-    vim.g.gruvbox_italics = 0
-    vim.g.gruvbox_plugin_hi_groups = 0
-    vim.opt.bg = 'dark'
-    require('modules.colors')
-end
