005c02734635198a7775cc6869768f74d0ccf4eb
Zachary Churchill <zacharyachurchill@gmail.com>
2022-09-27 21:21:31 -0400

    treesitter, incremental selection

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -174,6 +174,16 @@ local function packer_use(use)
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate',
+        config = function ()
+            require'nvim-treesitter.configs'.setup {
+                highlight = {
+                    enable = true,
+                    disable = { "haskell" }
+                },
+                textobjects = { enable = true },
+                incremental_selection = { enable = true, keymaps = { init_selection = '<CR>', scope_incremental = '<CR>', node_incremental = '<TAB>', node_decremental = '<S-TAB>', }, },
+            }
+        end
     }
 end
 
