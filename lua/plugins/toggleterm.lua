8f897be5df6de0499624e3eff62448dd3e7a242a
Zachary Churchill <zacharyachurchill@gmail.com>
2023-02-02 16:57:34 -0500

    toggleterm options

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,16 +3,21 @@ return function()
         direction = 'vertical',
         hide_numbers = true,
         insert_mappings = false,
+        -- n+open_mapping = open terminal n
         open_mapping = [[<Leader>t]],
         persist_size = true,
         shade_filetypes = {},
         shade_terminals = true,
         size = vim.o.columns * 0.5,
         start_in_insert = false,
+        shell = 'zsh',
         highlights = {
             Normal = {
                 link = 'DarkNormal'
             }
-        }
+        },
+        winbar = {
+            enabled = true,
+        },
     }
 end
