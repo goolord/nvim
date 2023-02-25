421f3e37d048f3ef15e2b6eae39f348b4ce5fa08
Zachary Churchill <zacharyachurchill@gmail.com>
2023-02-25 00:51:53 -0500

    telescope cursor theme

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -156,17 +156,22 @@ require("lazy").setup({
     -- better vim.ui
     {
         "stevearc/dressing.nvim",
-        lazy = true,
-        init = function()
-            vim.ui.select = function(...)
-                require("lazy").load({ plugins = { "dressing.nvim" } })
-                return vim.ui.select(...)
-            end
-            vim.ui.input = function(...)
-                require("lazy").load({ plugins = { "dressing.nvim" } })
-                return vim.ui.input(...)
-            end
-        end,
+        config = function ()
+            require('dressing').setup {
+                select = { telescope = require('telescope.themes').get_cursor({
+                        borderchars = {
+                            results =  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+                            prompt =  {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+                            preview =  {' ', ' ', ' ', '▏', '▏', ' ', ' ', '▏' };
+                        },
+                        layout_config = {
+                            width = 60
+                        },
+                        initial_mode = "normal",
+                    }),
+                }
+            }
+        end
     },
 
     {
