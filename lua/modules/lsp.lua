28ece87e353ff9f02302f6c4b2b490308eb53f00
Zachary Churchill <zacharyachurchill@gmail.com>
2025-01-13 18:12:42 -0500

    Revert "blink.cmp", too much is broken
    This reverts commit 8d1082b7034af7446c30b044e89ffe63b885904d.

--- lua/modules/lsp.lua
+++ lua/modules/lsp.lua
@@ -45,7 +45,6 @@ return function()
     end
 
     local capabilities = vim.lsp.protocol.make_client_capabilities()
-    capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
     capabilities.textDocument.completion.completionItem.snippetSupport = true
     capabilities.textDocument.completion.completionItem.resolveSupport = {
         properties = {
