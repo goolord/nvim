83cb28cb346ffa91bd36e4cf979f8107d3ec71dc
Zachary Churchill <zacharyachurchill@gmail.com>
2025-01-13 18:09:47 -0500

    blink.cmp

--- lua/modules/lsp.lua
+++ lua/modules/lsp.lua
@@ -45,6 +45,7 @@ return function()
     end
 
     local capabilities = vim.lsp.protocol.make_client_capabilities()
+    capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
     capabilities.textDocument.completion.completionItem.snippetSupport = true
     capabilities.textDocument.completion.completionItem.resolveSupport = {
         properties = {
